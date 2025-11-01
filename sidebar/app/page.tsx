e87106b4d190c7f2f5061935d3f386fee60ecbf0
Dammy <dammy2013@gmail.com>
2025-11-01 14:58:57 -0400

    Add sidebar demo component with shadcn UI

---
+++ sidebar/app/page.tsx
@@ -0,0 +1,9 @@
+import { SidebarDemo } from "@/components/sidebar-demo";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full bg-neutral-50 dark:bg-neutral-950 flex items-center justify-center p-4 md:p-10">
+      <SidebarDemo />
+    </div>
+  );
+}
